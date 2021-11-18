b27bab4e64372547aefd8423167848ec49ad6833
saraswathirg <4saraswathi95@gmail.com>
2021-11-19 01:17:08 +0530

    commands

---
+++ deploy_commands.groovy
@@ -0,0 +1,22 @@
+pipeline{
+    agent any
+    string(name:'BRANCH_NAME',defaultValue:'master')
+    string(name:'BRANCH_NUM')
+    string(name:;'SERVER_IP')
+}
+stages{
+    stage("downloading the artifact"){
+        steps{
+            println"here the artifact is downloaded"
+            sh "aws s3 ls"
+            sh "aws s3 ls s3://publicbucke"
+            sh "aws s3 ls s3://publicbucke/${BRANCH_NAME}/${BUILD_NUM}"
+            sh "aws s3 ls s3://publicbucke/${BRANCH_NAME}/${BUILD_NUM}/hello-${BUILD_NUM}.war"
+        }
+    }
+    stage("copying the artifact"){
+        steps{
+            print 
+        }
+    }
+}
