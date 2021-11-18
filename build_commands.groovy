9cfbd39c409dfeaa18af278666d06a4729fb6f11
saraswathirg <4saraswathi95@gmail.com>
2021-11-18 22:38:11 +0530

    added uploading artifacts

--- build_commands.groovy
+++ build_commands.groovy
@@ -14,16 +14,20 @@ stages{
                 userRemoteConfigs:[https://github.com/Saraswathirg/boxfuse-sample-java-war-hello.git]
             ]
             )
+            sh "ls -lart./*"
         }
     }
 stage("building the code using maven clean package"){
     steps{
         println"here the code is converted to machine readable code"
+        sh "mvn clean package"
+        sh "ls -l target/"
     }
 }
 stage("uploading artifacts"){
     steps{
         println"here the artifact is moved to s3"
+        sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://publicbucke/"
 }
 }
 }
