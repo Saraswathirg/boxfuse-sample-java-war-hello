fa0a179a856ac8135d7eb4605e17fb3745331495
saraswathirg <4saraswathi95@gmail.com>
2021-11-18 11:40:04 +0530

    added a file check

---
+++ new_file.groovy
@@ -0,0 +1,14 @@
+pipeline{
+    agent any
+    stages{
+        stage{
+            steps("adding the code"){
+                println"clone added"
+            }
+        }
+    satge{
+        steps("building code"){
+            println"code builded"
+        }
+    }
+}
