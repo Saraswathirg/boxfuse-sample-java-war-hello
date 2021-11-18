9ca5ff02af250907cd5cf82ed9eb46b86597af5b
saraswathirg <4saraswathi95@gmail.com>
2021-11-18 23:32:48 +0530

    added build

--- build_branch.groovy
+++ build_branch.groovy
@@ -23,7 +23,7 @@ pipeline{
         stage("uploading to artifacts"){
             steps{
                 println "here the artifact is moved to s3"
-                sh "aws s3 cp target/hello-${BUILD NUMBER}.war s3://publicbucke/"
+                sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://publicbucke/"
             }
         }
     }
