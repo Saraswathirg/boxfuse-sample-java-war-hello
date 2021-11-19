add708f6240b35a75c02352df41e369edebfa82f
saraswathirg <4saraswathi95@gmail.com>
2021-11-20 01:23:54 +0530

    added deploy steps

--- test_steps.groovy
+++ test_steps.groovy
@@ -2,6 +2,9 @@ pipeline{
     agent any
     parameters{
         string(name:'BRANCH_NAME',defaultValue:'master')
+        string(name:'BRANCH',defaultValue:'')
+        string(name:'BUILD_NUMBER',defaultValue:'')
+        string(name:'SERVER_IP',defaultValue:'')
     }
     stages{
         stage("build the code"){
@@ -31,11 +34,17 @@ pipeline{
         stage("download the artifact"){
             steps{
                 println"here the artifcat is downloaded"
+                sh"""
+                aws s3 ls
+                aws s3 ls s3://alltime
+                aws s3 ls s3://alltime/${BRANCH}/${BUILD_NUMBER}/
+                aws s3 cp s3://alltime/${BRANCH}/${BUILD_NUMBER}/hello-${BUILD_NUMBER}.war/ ."""
             }
         }
         stage("copy the artifact"){
             steps{
                 println"the artifact is copied"
+                sh "scp -i /tmp/awsaws.pem hello-${BUILD_NUMBER}.war ec2-user@${SERVER_IP}:/var/lib/tomcat/webspps"
             }
         }
     }
