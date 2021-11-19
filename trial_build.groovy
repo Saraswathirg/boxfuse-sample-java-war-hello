bad1a0cc2435350484fb9d6d9d33d8475b535aa7
saraswathirg <4saraswathi95@gmail.com>
2021-11-19 19:50:35 +0530

    checkout added

--- trial_build.groovy
+++ trial_build.groovy
@@ -4,6 +4,12 @@ pipeline{
         stage("clone the code"){
             steps{
                 println "here the clone is coded"
+                sh "ls -l"
+                checkout([
+                    $class:'GitSCM',
+                    branches:[[name:'*/master']],
+                    userRemoteConfigs:[[url'https://github.com/Saraswathirg/boxfuse-sample-java-war-hello.git']]
+                ])
             }
         }
         stage("convert the code"){
