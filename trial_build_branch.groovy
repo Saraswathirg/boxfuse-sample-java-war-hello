48272ef25825d64764d61cf0d863b3714f278385
saraswathirg <4saraswathi95@gmail.com>
2021-11-19 22:37:22 +0530

    added all steps commands

--- trial_build_branch.groovy
+++ trial_build_branch.groovy
@@ -4,16 +4,27 @@ pipeline{
         stage("build the code"){
             steps{
                 println"here the code is built"
+                sh "ls -l"
+                checkout([
+                    $class:'GitSCM',
+                    branches:[[name:'*/master']],
+                    userRemoteConfigs:[[url:'https://github.com/Saraswathirg/boxfuse-sample-java-war-hello.git']]
+                ])
+                sh "ls -lart ./*"
             }
         }
         stage("code converted"){
             steps{
                 println"the code is converted"
+                sh"mvn clean package"
+                sh "ls -l target/"
             }
         }
         stage("stored to s3"){
             steps{
                 println"the artifact is stored"
+                sh "echo$BUILD_NUMBER"
+                sh "aws s3 cp target/hello-${BUILD_NUMBER}.war s3://buildsteps"
             }
         }
     }
