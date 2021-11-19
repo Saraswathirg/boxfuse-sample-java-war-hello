34000e42cafb848319b50770d3196f651f1595e2
saraswathirg <4saraswathi95@gmail.com>
2021-11-19 23:25:05 +0530

    added a step

--- trial_deploy.groovy
+++ trial_deploy.groovy
@@ -1,9 +1,17 @@
 pipeline{
     agent any
+    parameters{
+        string(name:'BRANCH_NAME',defaultValue:)
+        string(name:'BUILD_NUMBER',defaultValue:'')
+    }
     stages{
         stage("download the artifacts"){
             steps{
                 println"here the artifact downloaded"
+                sh"""
+                aws s3 ls
+                aws s3 ls s3://buildsteps
+                aws s3 cp s3://buildsteps/${BRANCH_NAME}/${BUILD_NUMBER}/.
             }
         }
         stage("here the code is copied"){
