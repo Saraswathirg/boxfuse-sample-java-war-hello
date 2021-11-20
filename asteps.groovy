c451a01e25a218abf469b4394b6a64ebb4f49bec
saraswathirg <4saraswathi95@gmail.com>
2021-11-20 14:52:35 +0530

    added steps

---
+++ asteps.groovy
@@ -0,0 +1,31 @@
+pipeline{
+    agent any
+    stages{
+        stage("build the code"){
+            steps{
+                println "here the code is built"
+            }
+        }
+        stage("code is converted"){
+            steps{
+                println"here the code is converted"
+            }
+        }
+        stage("the artifact is stored"){
+            steps{
+                println"stored to s3"
+                }
+            }
+            stage("the code is downloaded"){
+                steps{
+                    println"downloaded from s3"
+                }
+            }
+            stage("the code is copied"){
+                steps{
+                    println"code copied"
+                }
+            }
+        }
+    }
+}
