f38fd9a1d3cfcaa0173ba592a783cca90c3575ea
saraswathirg <4saraswathi95@gmail.com>
2021-11-20 18:44:33 +0530

    added clone

--- AAprac_trial.groovy
+++ AAprac_trial.groovy
@@ -1,9 +1,18 @@
 pipeline{
     agent any
+    parameters{
+        string(name:'BRANCH',defaultValue:'master')
+    }
     stages{
         stage("clone the code"){
             steps{
                 println"the code is cloned"
+                sh "ls -l"
+                checkout([
+                    $class:'GitSCM',
+                    branches:[[name:'${BRANCH}']],
+                    userRemoteConfigs:[[url:'https://github.com/Saraswathirg/boxfuse-sample-java-war-hello.git']]
+                ])
             }
         }
         stage("build the code"){
