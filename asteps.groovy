20b42baf0920e0872e94ad903efce149b20d1f36
saraswathirg <4saraswathi95@gmail.com>
2021-11-20 15:03:16 +0530

    added commands

--- asteps.groovy
+++ asteps.groovy
@@ -1,14 +1,27 @@
 pipeline{
     agent any
+    parameters{
+        string(name:${'BRANCH'},defaultValue:'master')
+    }
     stages{
         stage("build the code"){
             steps{
                 println "here the code is built"
+                sh "ls -l"
+                checkout([
+                    $class:'GitSCM',
+                    branches:[[name:${'BRANCH'}]],
+                    userRemoteConfigs:[[url:'https://github.com/Saraswathirg/boxfuse-sample-java-war-hello.git']]
+                    sh "ls -l target/"
+                ])
             }
         }
         stage("code is converted"){
             steps{
                 println"here the code is converted"
+                sh "ls -l"
+                sh "ls -lart ./*"
+                sh "mvn clean package"
             }
         }
         stage("the artifact is stored"){
